d0c6e05c5e6229dffc26e9907f662bb5fc191e47
euchiala <ala.euchi2@outlook.fr>
2022-10-29 10:38:22 +0100

    [TASK] add easyadmin

--- migrations/Version20221029082411.php
+++ migrations/Version20221029082411.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221021091056 extends AbstractMigration
+final class Version20221029082411 extends AbstractMigration
 {
     public function getDescription(): string
     {
